548dc012e79c6636f4e51ca88ab137cd43ce2a43
Muhammad Zubair <m.zubaircs@gmail.com>
2016-11-22 17:46:46 +0500

    Created demo for input handling.

---
+++ PizzaTranslator/index.android.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  TextInput,
+  Text,
+  View
+} from 'react-native';
+
+export default class PizzaTranslator extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  render() {
+    return (
+       <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('PizzaTranslator', () => PizzaTranslator);
--- layoutFlexbox/README.md
+++ layoutFlexbox/README.md
@@ -1,7 +1,6 @@
 # Ract Native Mobile Apps:
 
-This demo is based on Layout with flex dimentions.  Use flex in a component's style to have the component expand and shrink dynamically based on available space. 
-A component can only expand to fill available space if its parent has dimensions greater than 0.
+This demo is based on Layout with flex dimentions. A component can specify the layout of its children using the flexbox algorithm. Flexbox is designed to provide a consistent layout on different screen sizes.
 
 
 #Getting Started :
